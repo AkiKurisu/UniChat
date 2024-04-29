659c19e41fd369c7c58496c645168cf008f8fbd6
AkiKurisu <2683987717@qq.com>
2024-04-29 16:54:10 +0800

    Restore llmtranslator
    Use inner chain to process llm input

---
+++ Runtime/Models/Translator/LLMTranslator.cs
@@ -0,0 +1,38 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Kurisu.UniChat.Chains;
+namespace Kurisu.UniChat.Translators
+{
+    public class LLMTranslator : ITranslator
+    {
+        private readonly StackableChain innerChain;
+        private string input;
+        /// <summary>
+        /// Create a translator using llm.
+        /// </summary>
+        /// <param name="chatModel">Set chat model to use</param>
+        /// <param name="sourceLanguage">Set source language</param>
+        /// <param name="targetLanguage">Set target language</param>
+        /// <param name="usePromptPreset">Set to use translator system prompt preset</param>
+        public LLMTranslator(ILargeLanguageModel llm, string sourceLanguage, string targetLanguage, string prompt = null)
+        {
+            if (string.IsNullOrEmpty(prompt))
+            {
+                if (!string.IsNullOrEmpty(sourceLanguage))
+                    prompt = "{targetLanguage} and {sourceLanguage} are language codes. You should translate {sourceLanguage} to {targetLanguage}. You should only reply the translation.\n{input}";
+                else
+                    prompt = "{targetLanguage} is language code. You should detect my language and translate them to {targetLanguage}. You should only reply the translation.\n{input}";
+            }
+            innerChain = Chain.Set(sourceLanguage, "sourceLanguage")
+                        | Chain.Set(targetLanguage, "targetLanguage")
+                        | Chain.Set(() => input, "input")
+                        | Chain.Template(prompt)
+                        | Chain.LLM(llm);
+        }
+        public async UniTask<string> TranslateAsync(string input, CancellationToken ct)
+        {
+            this.input = input;
+            return await innerChain.Run("text");
+        }
+    }
+}
